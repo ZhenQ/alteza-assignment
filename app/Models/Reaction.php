ec6fcdd94af35f27dcc2977f8eedcecf80395d75
zhen <zhen@fortelmedia.com>
2019-05-31 05:54:55 +0200

    initial commit

--- app/Models/Reaction.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\Models;
-
-class Reaction extends Model
-{
-    protected $tableName = 'Reactions';
-}
--- public/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require __DIR__ . '/../vendor/autoload.php';
-
-require __DIR__ . '/../config.php';
-require __DIR__ . '/../routes.php'; //register routes
-
-$app = new \App\Lib\App($config, $router);
